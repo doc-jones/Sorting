b26f73a40a1e5afed888f5cd694235e6035b159a
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-11-06 21:06:57 -0500

    Add selection_sort as non in-place selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,35 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+# TO-DO: Complete the selection_sort() function below
 
 
+def findSmallest(arr):
+    smallest = arr[0]  # Stores the smallest value
+    smallest_index = 0  # Stores the index of the smallest value
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:  # compare next value to smallest value
+            smallest = arr[i]  # value
+            smallest_index = i  # index
+    return smallest_index
 
-        # TO-DO: swap
 
-
-
-
+def selection_sort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)  # Find smallest element in arr
+        new_arr.append(arr.pop(smallest))  # and add to new_arr
+        new_arr = arr
     return arr
 
 
+# print(selection_sort([5, 3, 6, 2, 10]))
+
+
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
